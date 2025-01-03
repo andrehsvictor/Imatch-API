6e28003fc71fb4d5e1fefaca721962d503a05634
andrehsvictor <andrehsvictor@gmail.com>
2025-01-03 10:04:39 -0300

    feat: Initialize Spring Boot project

---
+++ src/main/java/andrehsvictor/imatch/ImatchApplication.java
@@ -0,0 +1,13 @@
+package andrehsvictor.imatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrehsvictor/imatch/ImatchApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.imatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
